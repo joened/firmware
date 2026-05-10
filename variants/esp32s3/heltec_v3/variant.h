5665b16d4b008cee837d762a86a917baa6afdc83
joened <arrive06@gmail.com>
2026-05-10 09:00:49 +0700

    heltec_Update variant.h
    v3

--- variants/esp32s3/heltec_v3/variant.h
+++ variants/esp32s3/heltec_v3/variant.h
@@ -20,25 +20,26 @@
 #define ADC_ATTENUATION ADC_ATTEN_DB_2_5 // lower dB for high resistance voltage divider
 #define ADC_MULTIPLIER 4.9 * 1.045
 
-#define USE_SX1262
+#define USE_SX1276
 
-#define LORA_DIO0 -1 // a No connect on the SX1262 module
-#define LORA_RESET 12
-#define LORA_DIO1 14 // SX1262 IRQ
-#define LORA_DIO2 13 // SX1262 BUSY
-#define LORA_DIO3    // Not connected on PCB, but internally on the TTGO SX1262, if DIO3 is high the TXCO is enabled
+#define LORA_DIO0 3 // a No connect on the SX1262 module
+#define LORA_RESET 9
+//#define LORA_DIO1 14 // SX1262 IRQ
+//#define LORA_DIO2 13 // SX1262 BUSY
+//#define LORA_DIO3    // Not connected on PCB, but internally on the TTGO SX1262, if DIO3 is high the TXCO is enabled
 
-#define LORA_SCK 9
-#define LORA_MISO 11
-#define LORA_MOSI 10
-#define LORA_CS 8
+#define LORA_SCK 12
+#define LORA_MISO 13
+#define LORA_MOSI 11
+#define LORA_CS 10
 
-#define SX126X_CS LORA_CS
-#define SX126X_DIO1 LORA_DIO1
-#define SX126X_BUSY LORA_DIO2
-#define SX126X_RESET LORA_RESET
+#define SX127X_CS LORA_CS
+//#define SX126X_DIO1 LORA_DIO1
+//#define SX126X_BUSY LORA_DIO2
+#define SX127X_RESET LORA_RESET
+#define SX127X_DIO0 LORA_DIO0
 
-#define SX126X_DIO2_AS_RF_SWITCH
-#define SX126X_DIO3_TCXO_VOLTAGE 1.8
+#define SX127X_DIO2_AS_RF_SWITCH
+#define SX127X_DIO3_TCXO_VOLTAGE 1.8
 
 #define HAS_32768HZ 1
